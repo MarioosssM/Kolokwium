90de6cd12479855bf36b56bde28adf04a1f8adba
MarioosssM <mariopet27@gmail.com>
2016-05-16 09:41:35 +0200

    added body in Fishes method

--- Kolokwium/Kolokwium/Program.cs
+++ Kolokwium/Kolokwium/Program.cs
@@ -8,9 +8,37 @@ namespace Kolokwium
 {
     public class Program
     {
-        public static int Fishes()
+        public static int Fishes(int[] A, int[] B)
         {
-            return 0;
+            Stack<int> aliveFish = new Stack<int>();
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (aliveFish.Count == 0)
+                {
+                    aliveFish.Push(i);
+                }
+                else
+                {
+                    while (aliveFish.Count !=0 && B[i] - B[aliveFish.Peek()] == -1 && A[i] > A[aliveFish.Peek()] )
+                    {
+                        aliveFish.Pop();
+                        if(aliveFish.Count != 0)
+                        {
+                            if(B[i] - B[aliveFish.Count] != -1)
+                            {
+
+                                aliveFish.Push(i);
+                            }
+                        }
+                        else
+                        {
+                            aliveFish.Push(i);
+                        }
+                    }
+                }
+            }
+
+            return aliveFish.Count;
         }
         static void Main(string[] args)
         {
